6f7426cbcf3e9c62ac155504d2f68b83d92da484
Crax <gsolimeno97@gmail.com>
2023-06-23 10:10:10 +0200

    introduce testbench App trait

---
+++ testbench/src/app.rs
@@ -0,0 +1,83 @@
+use engine::AppState;
+use log::trace;
+use nalgebra::*;
+use winit::{dpi::PhysicalSize, event::Event, event_loop::ControlFlow};
+
+pub trait App {
+    fn window_name() -> &'static str;
+
+    fn create(app_state: &AppState) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+
+    fn input(
+        &mut self,
+        app_state: &AppState,
+        event: winit::event::DeviceEvent,
+    ) -> anyhow::Result<()>;
+    fn update(&mut self, app_state: &mut AppState) -> anyhow::Result<()>;
+    fn draw(&mut self, app_state: &mut AppState) -> anyhow::Result<()>;
+}
+
+pub fn app_loop<A: App + 'static>(
+    app: &mut A,
+    event: Event<'_, ()>,
+) -> anyhow::Result<ControlFlow> {
+    let app_state_mut = engine::app_state_mut();
+    match event {
+        winit::event::Event::NewEvents(_) => {}
+        winit::event::Event::WindowEvent { event, .. } => match event {
+            winit::event::WindowEvent::CloseRequested => {
+                return Ok(ControlFlow::ExitWithCode(0));
+            }
+            winit::event::WindowEvent::Resized(_) => {
+                app_state_mut.swapchain.recreate_swapchain().unwrap();
+            }
+            _ => {}
+        },
+        winit::event::Event::DeviceEvent { event, .. } => {
+            app.input(app_state_mut, event)?;
+        }
+        winit::event::Event::UserEvent(_) => {}
+        winit::event::Event::Suspended => {}
+        winit::event::Event::Resumed => {}
+        winit::event::Event::MainEventsCleared => {
+            app_state_mut.swapchain.window.request_redraw();
+        }
+        winit::event::Event::RedrawRequested(..) => {
+            app_state_mut.begin_frame().unwrap();
+            app.update(app_state_mut)?;
+            app.draw(app_state_mut)?;
+            app_state_mut.end_frame().unwrap();
+        }
+        winit::event::Event::RedrawEventsCleared => {}
+        winit::event::Event::LoopDestroyed => app_state_mut.gpu.wait_device_idle().unwrap(),
+    }
+
+    Ok(ControlFlow::Poll)
+}
+
+pub fn bootstrap<A: App + 'static>() -> anyhow::Result<()> {
+    env_logger::init();
+    let event_loop = winit::event_loop::EventLoop::default();
+    let window = winit::window::WindowBuilder::default()
+        .with_inner_size(PhysicalSize {
+            width: 1240,
+            height: 720,
+        })
+        .build(&event_loop)?;
+
+    engine::init(A::window_name(), window)?;
+
+    let app = Box::new(A::create(engine::app_state())?);
+    let app = Box::leak(app);
+
+    trace!("Created app {}", A::window_name());
+
+    event_loop.run(move |event, _, control_flow| match app_loop(app, event) {
+        Ok(flow) => {
+            *control_flow = flow;
+        }
+        Err(e) => panic!("In main body of application: {}", e),
+    })
+}
--- testbench/src/planes.rs
+++ testbench/src/planes.rs
@@ -1,7 +1,9 @@
+mod app;
 mod utils;
 
 use std::{io::BufReader, mem::size_of, rc::Rc};
 
+use app::{bootstrap, App};
 use ash::vk::{BufferUsageFlags, PresentModeKHR};
 
 use gpu::{BufferCreateInfo, MemoryDomain};
@@ -12,8 +14,7 @@ use engine::{
 };
 use nalgebra::*;
 use resource_map::ResourceMap;
-use winit::{dpi::PhysicalSize, event::ElementState, event_loop::ControlFlow};
-
+use winit::event::ElementState;
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct VertexData {
@@ -21,290 +22,238 @@ struct VertexData {
     pub color: Vector3<f32>,
     pub uv: Vector2<f32>,
 }
-fn main() -> anyhow::Result<()> {
-    env_logger::init();
+const SPEED: f32 = 0.1;
+const ROTATION_SPEED: f32 = 3.0;
+const MIN_DELTA: f32 = 1.0;
+pub struct PlanesApp {
+    resource_map: Rc<ResourceMap>,
+    camera: Camera,
+    forward_movement: f32,
+    rotation_movement: f32,
+    rot_x: f32,
+    rot_z: f32,
+    dist: f32,
+    movement: Vector3<f32>,
+    scene_renderer: DeferredRenderingPipeline,
+    scene: Scene,
+}
 
-    let resource_map = Rc::new(ResourceMap::new());
+impl App for PlanesApp {
+    fn window_name() -> &'static str {
+        "planes"
+    }
 
-    let event_loop = winit::event_loop::EventLoop::default();
-    let window = winit::window::WindowBuilder::default()
-        .with_inner_size(PhysicalSize {
-            width: 1240,
-            height: 720,
+    fn create(app_state: &engine::AppState) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        let resource_map = Rc::new(ResourceMap::new());
+
+        let camera = Camera {
+            location: point![2.0, 2.0, 2.0],
+            forward: vector![0.0, -1.0, -1.0].normalize(),
+            ..Default::default()
+        };
+
+        let forward_movement = 0.0;
+        let rotation_movement = 0.0;
+
+        let rot_x = 45.0;
+        let rot_z = 55.0;
+        let dist = 5.0;
+
+        let movement: Vector3<f32> = vector![0.0, 0.0, 0.0];
+        let cpu_image = image::load(
+            BufReader::new(std::fs::File::open("images/texture.jpg")?),
+            image::ImageFormat::Jpeg,
+        )?;
+        let cpu_image = cpu_image.into_rgba8();
+
+        let vertex_module =
+            utils::read_file_to_vk_module(&app_state.gpu, "./shaders/vertex_deferred.spirv")?;
+        let fragment_module =
+            utils::read_file_to_vk_module(&app_state.gpu, "./shaders/fragment_deferred.spirv")?;
+
+        let mesh_data = MeshCreateInfo {
+            label: Some("Quad mesh"),
+            indices: &[0, 1, 2, 2, 3, 0],
+            positions: &[
+                vector![-0.5, -0.5, 0.0],
+                vector![0.5, -0.5, 0.0],
+                vector![0.5, 0.5, 0.0],
+                vector![-0.5, 0.5, 0.0],
+            ],
+            colors: &[
+                vector![1.0, 0.0, 0.0],
+                vector![0.0, 1.0, 0.0],
+                vector![0.0, 0.0, 1.0],
+                vector![1.0, 1.0, 1.0],
+            ],
+            normals: &[
+                vector![0.0, 1.0, 0.0],
+                vector![0.0, 1.0, 0.0],
+                vector![0.0, 1.0, 0.0],
+                vector![0.0, 1.0, 0.0],
+            ],
+            tangents: &[
+                vector![0.0, 0.0, 1.0],
+                vector![0.0, 0.0, 1.0],
+                vector![0.0, 0.0, 1.0],
+                vector![0.0, 0.0, 1.0],
+            ],
+            uvs: &[
+                vector![1.0, 0.0],
+                vector![0.0, 0.0],
+                vector![0.0, 1.0],
+                vector![1.0, 1.0],
+            ],
+        };
+
+        let mesh = Mesh::new(&app_state.gpu, &mesh_data)?;
+        let mesh = resource_map.add(mesh);
+
+        let texture = Texture::new_with_data(
+            &app_state.gpu,
+            cpu_image.width(),
+            cpu_image.height(),
+            &cpu_image,
+            Some("Quad texture david"),
+        )?;
+        let texture = resource_map.add(texture);
+
+        let screen_quad_module =
+            utils::read_file_to_vk_module(&app_state.gpu, "./shaders/screen_quad.spirv")?;
+        let gbuffer_combine_module =
+            utils::read_file_to_vk_module(&app_state.gpu, "./shaders/gbuffer_combine.spirv")?;
+        let texture_copy_module =
+            utils::read_file_to_vk_module(&app_state.gpu, "./shaders/texture_copy.spirv")?;
+
+        let scene_renderer = DeferredRenderingPipeline::new(
+            &app_state.gpu,
+            resource_map.clone(),
+            screen_quad_module,
+            gbuffer_combine_module,
+            texture_copy_module,
+        )?;
+
+        let material = scene_renderer.get_context().create_material(
+            &app_state.gpu,
+            &resource_map,
+            MaterialDescription {
+                domain: MaterialDomain::Surface,
+                uniform_buffers: vec![],
+                input_textures: vec![texture.clone()],
+                fragment_module: &fragment_module,
+                vertex_module: &vertex_module,
+            },
+        )?;
+        let material = resource_map.add(material);
+
+        engine::app_state_mut()
+            .swapchain
+            .select_present_mode(PresentModeKHR::MAILBOX)?;
+
+        let mut scene = Scene::new();
+
+        scene.add(ScenePrimitive {
+            mesh: mesh.clone(),
+            material: material.clone(),
+            transform: Matrix4::identity(),
+        });
+        scene.add(ScenePrimitive {
+            mesh: mesh.clone(),
+            material: material.clone(),
+            transform: Matrix4::new_translation(&vector![0.0, 0.0, 1.0]),
+        });
+        scene.add(ScenePrimitive {
+            mesh: mesh.clone(),
+            material: material.clone(),
+            transform: Matrix4::new_translation(&vector![0.0, 0.0, -1.0]),
+        });
+        Ok(Self {
+            resource_map,
+            camera,
+            forward_movement,
+            rotation_movement,
+            rot_x,
+            rot_z,
+            dist,
+            movement,
+            scene_renderer,
+            scene,
         })
-        .build(&event_loop)?;
+    }
 
-    engine::init("Hello World!", window)?;
-
-    let cpu_image = image::load(
-        BufReader::new(std::fs::File::open("images/texture.jpg")?),
-        image::ImageFormat::Jpeg,
-    )?;
-    let cpu_image = cpu_image.into_rgba8();
-
-    let vertex_module =
-        utils::read_file_to_vk_module(&engine::app_state().gpu, "./shaders/vertex_deferred.spirv")?;
-    let fragment_module = utils::read_file_to_vk_module(
-        &engine::app_state().gpu,
-        "./shaders/fragment_deferred.spirv",
-    )?;
-
-    let mesh_data = MeshCreateInfo {
-        label: Some("Quad mesh"),
-        indices: &[0, 1, 2, 2, 3, 0],
-        positions: &[
-            vector![-0.5, -0.5, 0.0],
-            vector![0.5, -0.5, 0.0],
-            vector![0.5, 0.5, 0.0],
-            vector![-0.5, 0.5, 0.0],
-        ],
-        colors: &[
-            vector![1.0, 0.0, 0.0],
-            vector![0.0, 1.0, 0.0],
-            vector![0.0, 0.0, 1.0],
-            vector![1.0, 1.0, 1.0],
-        ],
-        normals: &[
-            vector![0.0, 1.0, 0.0],
-            vector![0.0, 1.0, 0.0],
-            vector![0.0, 1.0, 0.0],
-            vector![0.0, 1.0, 0.0],
-        ],
-        tangents: &[
-            vector![0.0, 0.0, 1.0],
-            vector![0.0, 0.0, 1.0],
-            vector![0.0, 0.0, 1.0],
-            vector![0.0, 0.0, 1.0],
-        ],
-        uvs: &[
-            vector![1.0, 0.0],
-            vector![0.0, 0.0],
-            vector![0.0, 1.0],
-            vector![1.0, 1.0],
-        ],
-    };
-
-    let mesh = Mesh::new(&engine::app_state().gpu, &mesh_data)?;
-    let mesh = resource_map.add(mesh);
-
-    let vertex_data = &[
-        VertexData {
-            position: vector![-0.5, -0.5],
-            color: vector![1.0, 0.0, 0.0],
-            uv: vector![1.0, 0.0],
-        },
-        VertexData {
-            position: vector![0.5, -0.5],
-            color: vector![0.0, 1.0, 0.0],
-            uv: vector![0.0, 0.0],
-        },
-        VertexData {
-            position: vector![0.5, 0.5],
-            color: vector![0.0, 0.0, 1.0],
-            uv: vector![0.0, 1.0],
-        },
-        VertexData {
-            position: vector![-0.5, 0.5],
-            color: vector![1.0, 1.0, 1.0],
-            uv: vector![1.0, 1.0],
-        },
-    ];
-    let indices: &[u32] = &[0, 1, 2, 2, 3, 0];
-    let data_size = size_of::<VertexData>() * vertex_data.len();
-    let vertex_buffer = {
-        let create_info = BufferCreateInfo {
-            label: Some("Example Vertex Buffer"),
-            size: data_size,
-            usage: BufferUsageFlags::VERTEX_BUFFER | BufferUsageFlags::TRANSFER_DST,
-        };
-        let buffer = engine::app_state()
-            .gpu
-            .create_buffer(&create_info, MemoryDomain::DeviceLocal)?;
-        buffer
-    };
-
-    let index_size = size_of::<u32>() * indices.len();
-
-    let index_buffer = {
-        let create_info = BufferCreateInfo {
-            size: index_size,
-            usage: BufferUsageFlags::INDEX_BUFFER | BufferUsageFlags::TRANSFER_DST,
-            label: Some("Example Index Buffer"),
-        };
-        let buffer = engine::app_state()
-            .gpu
-            .create_buffer(&create_info, MemoryDomain::DeviceLocal)?;
-        buffer
-    };
-
-    let texture = Texture::new_with_data(
-        &engine::app_state().gpu,
-        cpu_image.width(),
-        cpu_image.height(),
-        &cpu_image,
-        Some("Quad texture david"),
-    )?;
-    let texture = resource_map.add(texture);
-
-    engine::app_state()
-        .gpu
-        .write_buffer_data(&vertex_buffer, vertex_data)?;
-    engine::app_state()
-        .gpu
-        .write_buffer_data(&index_buffer, indices)?;
-
-    let screen_quad_module =
-        utils::read_file_to_vk_module(&engine::app_state().gpu, "./shaders/screen_quad.spirv")?;
-    let gbuffer_combine_module =
-        utils::read_file_to_vk_module(&engine::app_state().gpu, "./shaders/gbuffer_combine.spirv")?;
-    let texture_copy_module =
-        utils::read_file_to_vk_module(&engine::app_state().gpu, "./shaders/texture_copy.spirv")?;
-
-    let mut scene_renderer = DeferredRenderingPipeline::new(
-        &engine::app_state().gpu,
-        resource_map.clone(),
-        screen_quad_module,
-        gbuffer_combine_module,
-        texture_copy_module,
-    )?;
-
-    let material_1 = scene_renderer.get_context().create_material(
-        &engine::app_state().gpu,
-        &resource_map,
-        MaterialDescription {
-            domain: MaterialDomain::Surface,
-            uniform_buffers: vec![],
-            input_textures: vec![texture.clone()],
-            fragment_module: &fragment_module,
-            vertex_module: &vertex_module,
-        },
-    )?;
-    let material = resource_map.add(material_1);
-
-    engine::app_state_mut()
-        .swapchain
-        .select_present_mode(PresentModeKHR::MAILBOX)?;
-
-    let mut scene = Scene::new();
-
-    scene.add(ScenePrimitive {
-        mesh: mesh.clone(),
-        material: material.clone(),
-        transform: Matrix4::identity(),
-    });
-    scene.add(ScenePrimitive {
-        mesh: mesh.clone(),
-        material: material.clone(),
-        transform: Matrix4::new_translation(&vector![0.0, 0.0, 1.0]),
-    });
-    scene.add(ScenePrimitive {
-        mesh: mesh.clone(),
-        material: material.clone(),
-        transform: Matrix4::new_translation(&vector![0.0, 0.0, -1.0]),
-    });
-
-    let mut camera = Camera {
-        location: point![2.0, 2.0, 2.0],
-        forward: vector![0.0, -1.0, -1.0].normalize(),
-        ..Default::default()
-    };
-
-    let mut forward_movement = 0.0;
-    let mut rotation_movement = 0.0;
-
-    let mut rot_x = 45.0;
-    let mut rot_z = 55.0;
-    let mut dist = 5.0;
-
-    let mut movement: Vector3<f32> = vector![0.0, 0.0, 0.0];
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-
-        const SPEED: f32 = 0.1;
-        const ROTATION_SPEED: f32 = 3.0;
-        const MIN_DELTA: f32 = 1.0;
-
+    fn input(
+        &mut self,
+        app_state: &engine::AppState,
+        event: winit::event::DeviceEvent,
+    ) -> anyhow::Result<()> {
         match event {
-            winit::event::Event::NewEvents(_) => {}
-            winit::event::Event::WindowEvent { event, .. } => match event {
-                winit::event::WindowEvent::CloseRequested => {
-                    *control_flow = ControlFlow::ExitWithCode(0)
-                }
-                winit::event::WindowEvent::Resized(_) => {
-                    engine::app_state_mut()
-                        .swapchain
-                        .recreate_swapchain()
-                        .unwrap();
-                }
-                _ => {}
-            },
-            winit::event::Event::DeviceEvent { event, .. } => match event {
-                winit::event::DeviceEvent::Button { button, state } => {
-                    let mul = if state == ElementState::Pressed {
-                        1.0
-                    } else {
-                        0.0
-                    };
-                    if button == 3 {
-                        rotation_movement = mul;
-                    } else if button == 1 {
-                        forward_movement = mul;
-                    }
-                }
-                winit::event::DeviceEvent::MouseMotion { delta } => {
-                    movement.x = (delta.0.abs() as f32 - MIN_DELTA).max(0.0)
-                        * delta.0.signum() as f32
-                        * ROTATION_SPEED;
-                    movement.y = (delta.1.abs() as f32 - MIN_DELTA).max(0.0)
-                        * delta.1.signum() as f32 as f32
-                        * ROTATION_SPEED;
-                }
-                _ => {}
-            },
-            winit::event::Event::UserEvent(_) => {}
-            winit::event::Event::Suspended => {}
-            winit::event::Event::Resumed => {}
-            winit::event::Event::MainEventsCleared => {
-                engine::app_state().swapchain.window.request_redraw();
-            }
-            winit::event::Event::RedrawRequested(..) => {
-                if rotation_movement > 0.0 {
-                    rot_z += movement.y;
-                    rot_z = rot_z.clamp(-89.0, 89.0);
-                    rot_x += movement.x;
+            winit::event::DeviceEvent::Button { button, state } => {
+                let mul = if state == ElementState::Pressed {
+                    1.0
                 } else {
-                    dist += movement.y * forward_movement * SPEED;
+                    0.0
+                };
+                if button == 3 {
+                    self.rotation_movement = mul;
+                } else if button == 1 {
+                    self.forward_movement = mul;
                 }
-
-                let new_forward = Rotation::<f32, 3>::from_axis_angle(
-                    &Unit::new_normalize(vector![0.0, 0.0, 1.0]),
-                    rot_x.to_radians(),
-                ) * Rotation::<f32, 3>::from_axis_angle(
-                    &Unit::new_normalize(vector![0.0, 1.0, 0.0]),
-                    -rot_z.to_radians(),
-                );
-                let new_forward = new_forward.to_homogeneous();
-                let new_forward = new_forward.column(0);
-
-                let direction = vector![new_forward[0], new_forward[1], new_forward[2]];
-                let new_position = direction * dist;
-                let new_position = point![new_position.x, new_position.y, new_position.z];
-                camera.location = new_position;
-
-                let direction = vector![new_forward[0], new_forward[1], new_forward[2]];
-                camera.forward = -direction;
-
-                let app_state_mut = engine::app_state_mut();
-                app_state_mut.begin_frame().unwrap();
-                scene_renderer
-                    .render(&camera, &scene, &mut app_state_mut.swapchain)
-                    .unwrap();
-                app_state_mut.end_frame().unwrap();
             }
-            winit::event::Event::RedrawEventsCleared => {}
-            winit::event::Event::LoopDestroyed => {
-                engine::app_state().gpu.wait_device_idle().unwrap()
+
+            winit::event::DeviceEvent::MouseMotion { delta } => {
+                self.movement.x = (delta.0.abs() as f32 - MIN_DELTA).max(0.0)
+                    * delta.0.signum() as f32
+                    * ROTATION_SPEED;
+                self.movement.y = (delta.1.abs() as f32 - MIN_DELTA).max(0.0)
+                    * delta.1.signum() as f32 as f32
+                    * ROTATION_SPEED;
             }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn draw(&mut self, app_state: &mut engine::AppState) -> anyhow::Result<()> {
+        self.scene_renderer
+            .render(&self.camera, &self.scene, &mut app_state.swapchain)
+            .unwrap();
+
+        Ok(())
+    }
+
+    fn update(&mut self, _app_state: &mut engine::AppState) -> anyhow::Result<()> {
+        if self.rotation_movement > 0.0 {
+            self.rot_z += self.movement.y;
+            self.rot_z = self.rot_z.clamp(-89.0, 89.0);
+            self.rot_x += self.movement.x;
+        } else {
+            self.dist += self.movement.y * self.forward_movement * SPEED;
         }
-    })
+
+        let new_forward = Rotation::<f32, 3>::from_axis_angle(
+            &Unit::new_normalize(vector![0.0, 0.0, 1.0]),
+            self.rot_x.to_radians(),
+        ) * Rotation::<f32, 3>::from_axis_angle(
+            &Unit::new_normalize(vector![0.0, 1.0, 0.0]),
+            -self.rot_z.to_radians(),
+        );
+        let new_forward = new_forward.to_homogeneous();
+        let new_forward = new_forward.column(0);
+
+        let direction = vector![new_forward[0], new_forward[1], new_forward[2]];
+        let new_position = direction * self.dist;
+        let new_position = point![new_position.x, new_position.y, new_position.z];
+        self.camera.location = new_position;
+
+        let direction = vector![new_forward[0], new_forward[1], new_forward[2]];
+        self.camera.forward = -direction;
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    bootstrap::<PlanesApp>()
 }
