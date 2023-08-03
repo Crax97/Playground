6f7c55067d5701e844693a35a24bb975a6e59e6e
Crax <gsolimeno97@gmail.com>
2023-08-03 11:53:11 +0200

    todo push of compute sample

---
+++ testbench/src/compute.rs
@@ -0,0 +1,20 @@
+﻿use gpu::{Gpu, GpuConfiguration};
+
+fn main() -> anyhow::Result<()> {
+    
+    
+    let gpu = Gpu::new(GpuConfiguration {
+        app_name: "compute sample",
+        engine_name: "compute engine",
+        pipeline_cache_path: None,
+        enable_debug_utilities: true,
+        window: todo!(),
+    });
+    
+    /*
+        let buffer = create_buffer();
+    */
+    
+    
+    Ok(())
+}
