47eb7a493e373045012c500aa783c25226999b52
Crax97 <gsolimeno97@gmail.com>
2023-11-17 21:54:47 +0100

    Load common resources

--- engine/src/bevy_ecs_app.rs
+++ engine/src/bevy_ecs_app.rs
@@ -2,14 +2,17 @@ use std::ops::{Deref, DerefMut};
 
 use bevy_ecs::{
     schedule::{Schedule, ScheduleLabel},
+    system::Resource,
     world::World,
 };
+use nalgebra::vector;
 use winit::event_loop::EventLoop;
 
 use crate::{
     app::{App, ImguiConsole},
     input::InputState,
-    utils, Camera, CvarManager, DeferredRenderingPipeline, RenderingPipeline, ResourceMap, Scene,
+    resource_map, utils, Camera, CvarManager, DeferredRenderingPipeline, Mesh, MeshCreateInfo,
+    MeshPrimitiveCreateInfo, RenderingPipeline, ResourceHandle, ResourceMap, Scene, Texture,
 };
 
 #[derive(ScheduleLabel, Debug, Hash, Copy, Clone, Eq, PartialEq, PartialOrd, Ord)]
@@ -18,10 +21,14 @@ struct StartupSchedule;
 #[derive(ScheduleLabel, Debug, Hash, Copy, Clone, Eq, PartialEq, PartialOrd, Ord)]
 struct UpdateSchedule;
 
+#[derive(ScheduleLabel, Debug, Hash, Copy, Clone, Eq, PartialEq, PartialOrd, Ord)]
+struct PostUpdateSchedule;
+
 pub struct BevyEcsApp {
     world: World,
     startup_schedule: Schedule,
     update_schedule: Schedule,
+    post_update_schedule: Schedule,
     imgui_console: ImguiConsole,
     renderer: DeferredRenderingPipeline,
 }
@@ -29,6 +36,15 @@ pub struct BevyEcsApp {
 pub struct BevyEcsAppWithLoop {
     app: BevyEcsApp,
     evt_loop: EventLoop<()>,
+}
+
+#[derive(Resource)]
+pub struct CommonResources {
+    pub cube_mesh: ResourceHandle<Mesh>,
+    pub quad_mesh: ResourceHandle<Mesh>,
+
+    pub white_texture: ResourceHandle<Texture>,
+    pub black_texture: ResourceHandle<Texture>,
 }
 
 impl BevyEcsApp {
@@ -48,6 +64,86 @@ impl BevyEcsApp {
 
     pub fn update_schedule(&mut self) -> &mut Schedule {
         &mut self.update_schedule
+    }
+
+    pub fn post_update_schedule(&mut self) -> &mut Schedule {
+        &mut self.post_update_schedule
+    }
+
+    fn create_common_resources(
+        gpu: &gpu::VkGpu,
+        resource_map: &mut ResourceMap,
+    ) -> anyhow::Result<CommonResources> {
+        let quad_mesh = {
+            let mesh_data = MeshCreateInfo {
+                label: Some("Quad mesh"),
+                primitives: &[MeshPrimitiveCreateInfo {
+                    indices: vec![0, 1, 2, 2, 3, 0],
+                    positions: vec![
+                        vector![-0.5, -0.5, 0.0],
+                        vector![0.5, -0.5, 0.0],
+                        vector![0.5, 0.5, 0.0],
+                        vector![-0.5, 0.5, 0.0],
+                    ],
+                    colors: vec![
+                        vector![1.0, 0.0, 0.0],
+                        vector![0.0, 1.0, 0.0],
+                        vector![0.0, 0.0, 1.0],
+                        vector![1.0, 1.0, 1.0],
+                    ],
+                    normals: vec![
+                        vector![0.0, 1.0, 0.0],
+                        vector![0.0, 1.0, 0.0],
+                        vector![0.0, 1.0, 0.0],
+                        vector![0.0, 1.0, 0.0],
+                    ],
+                    tangents: vec![
+                        vector![0.0, 0.0, 1.0],
+                        vector![0.0, 0.0, 1.0],
+                        vector![0.0, 0.0, 1.0],
+                        vector![0.0, 0.0, 1.0],
+                    ],
+                    uvs: vec![
+                        vector![1.0, 0.0],
+                        vector![0.0, 0.0],
+                        vector![0.0, 1.0],
+                        vector![1.0, 1.0],
+                    ],
+                }],
+            };
+
+            let mesh = Mesh::new(gpu, &mesh_data)?;
+            resource_map.add(mesh)
+        };
+
+        let cube_mesh = utils::load_cube_to_resource_map(gpu, resource_map)?;
+        let white_texture = Texture::new_with_data(
+            gpu,
+            1,
+            1,
+            &[0, 0, 0, 255],
+            Some("White texture"),
+            gpu::ImageFormat::Rgba8,
+            gpu::ImageViewType::Type2D,
+        )?;
+        let black_texture = Texture::new_with_data(
+            gpu,
+            1,
+            1,
+            &[255, 255, 255, 255],
+            Some("Black texture"),
+            gpu::ImageFormat::Rgba8,
+            gpu::ImageViewType::Type2D,
+        )?;
+        let white_texture = resource_map.add(white_texture);
+        let black_texture = resource_map.add(black_texture);
+
+        Ok(CommonResources {
+            cube_mesh,
+            quad_mesh,
+            white_texture,
+            black_texture,
+        })
     }
 }
 
@@ -77,16 +173,21 @@ impl App for BevyEcsApp {
         )?;
         let startup_schedule = Schedule::new(StartupSchedule);
         let update_schedule = Schedule::new(UpdateSchedule);
+        let post_update_schedule = Schedule::new(PostUpdateSchedule);
+
+        let default_resources = Self::create_common_resources(&app_state.gpu, &mut resource_map)?;
 
         world.insert_resource(resource_map);
         world.insert_resource(cvar_manager);
         world.insert_resource(input);
         world.insert_resource(console_writer);
+        world.insert_resource(default_resources);
 
         Ok(Self {
             world,
             startup_schedule,
             update_schedule,
+            post_update_schedule,
             imgui_console,
             renderer,
         })
@@ -128,6 +229,7 @@ impl App for BevyEcsApp {
             &mut self.world.get_resource_mut::<CvarManager>().unwrap(),
         );
         self.update_schedule.run(&mut self.world);
+        self.post_update_schedule.run(&mut self.world);
         Ok(())
     }
 
--- platform2dgame/src/main.rs
+++ platform2dgame/src/main.rs
@@ -1,8 +1,10 @@
 use engine::app::app_state::app_state;
 use engine::app::ConsoleWriter;
 use engine::bevy_ecs::entity::Entity;
+use engine::bevy_ecs::schedule::IntoSystemConfigs;
 use engine::bevy_ecs::system::{Query, Res, ResMut};
-use engine::{bevy_ecs, Time};
+use engine::components::{rendering_system, MeshComponent};
+use engine::{bevy_ecs, Camera, ResourceMap, Texture, Time};
 use engine::{
     bevy_ecs::{component::Component, system::Commands},
     BevyEcsApp,
@@ -18,9 +20,16 @@ fn main() -> anyhow::Result<()> {
         .add_systems((hello_system, print_system));
 
     app.update_schedule().add_systems(greet_system);
+    app.post_update_schedule()
+        .add_systems((camera_system, rendering_system.after(camera_system)));
 
     app.run()
 }
+
+fn setup_sprite_system(mut resource_map: ResMut<ResourceMap>, mut commands: Commands) {
+    // let texture = resource_map.load::<Texture>("images/texture.jpg", ImageLoader);
+}
+
 fn print_system(mut console_writer: ResMut<ConsoleWriter>) {
     println!("Hello world!");
     console_writer.write_message("Hello world!").unwrap();
@@ -29,6 +38,10 @@ fn hello_system(mut commands: Commands) {
     commands.spawn(Name("John".to_owned()));
     commands.spawn(Name("Marc".to_owned()));
     commands.spawn(Name("Jaques".to_owned()));
+}
+
+fn camera_system(mut commands: Commands) {
+    commands.insert_resource(Camera::new_orthographic(100.0, 100.0, 0.0001, 1000.0));
 }
 
 fn greet_system(query: Query<(Entity, &Name)>, mut writer: ResMut<ConsoleWriter>) {
