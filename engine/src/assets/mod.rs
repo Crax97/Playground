f08618a30a92e5bf87f0186ce7e966059974c467
Crax97 <gsolimeno97@gmail.com>
2024-05-31 22:19:25 +0200

    Add support for mesh loading

--- engine/src/assets/mod.rs
+++ engine/src/assets/mod.rs
@@ -1,11 +1,13 @@
 use std::io::{BufReader, Read};
 
 use anyhow::Context;
+use glam::{vec2, vec3, Vec3};
 use mgpu::Extents2D;
-use serde::{de::IntoDeserializer, Deserialize, Serialize};
+use russimp::scene::PostProcess;
+use serde::{Deserialize, Serialize};
 use texture::{TextureDescription, TextureSamplerConfiguration, TextureUsageFlags};
 
-use crate::asset_map::{Asset, AssetMap, LoadContext};
+use crate::asset_map::{Asset, AssetMap};
 
 use self::{material::Material, mesh::Mesh, texture::Texture};
 
@@ -14,8 +16,13 @@ pub mod mesh;
 pub mod shader;
 pub mod texture;
 
+#[derive(Serialize, Deserialize)]
+pub struct MeshMetadata {
+    pub source: String,
+}
+
 impl Asset for Mesh {
-    type Metadata = ();
+    type Metadata = MeshMetadata;
     fn asset_type_name() -> &'static str {
         "Mesh"
     }
@@ -29,11 +36,76 @@ impl Asset for Mesh {
         device.destroy_buffer(self.color_component).unwrap();
     }
 
-    fn import(_base_id: &str, _metadata: Self::Metadata, _ctx: &mut AssetMap) -> anyhow::Result<()>
+    fn import(base_id: &str, metadata: Self::Metadata, ctx: &mut AssetMap) -> anyhow::Result<()>
     where
         Self: Sized,
     {
-        unimplemented!()
+        use russimp::scene::Scene as RussimpScene;
+        let scene = RussimpScene::from_file(
+            &metadata.source,
+            vec![
+                PostProcess::CalculateTangentSpace,
+                PostProcess::Triangulate,
+                PostProcess::GenerateNormals,
+            ],
+        )?;
+        let mesh = &scene.meshes[0];
+
+        let indices = mesh
+            .faces
+            .iter()
+            .flat_map(|face| face.0.iter().copied())
+            .collect::<Vec<_>>();
+
+        let positions = mesh
+            .vertices
+            .iter()
+            .map(|v| vec3(v.x, v.y, v.z))
+            .collect::<Vec<_>>();
+
+        let normals = mesh
+            .normals
+            .iter()
+            .map(|v| vec3(v.x, v.y, v.z))
+            .collect::<Vec<_>>();
+
+        let tangents = mesh
+            .tangents
+            .iter()
+            .map(|v| vec3(v.x, v.y, v.z))
+            .collect::<Vec<_>>();
+
+        let uvs = mesh.texture_coords[0]
+            .as_ref()
+            .unwrap()
+            .iter()
+            .map(|v| vec2(v.x, v.y))
+            .collect::<Vec<_>>();
+
+        let colors = mesh.colors[0]
+            .as_ref()
+            .map(|c| c.iter().map(|v| vec3(v.r, v.g, v.b)).collect::<Vec<_>>())
+            .unwrap_or(vec![Vec3::ZERO]);
+
+        debug_assert!(
+            !indices.is_empty(),
+            "Meshes without index buffers aren't supported yet"
+        );
+
+        let desc = mesh::MeshDescription {
+            label: Some(base_id),
+            indices: &indices,
+            positions: &positions,
+            normals: &normals,
+            tangents: &tangents,
+            uvs: &uvs,
+            colors: &colors,
+        };
+
+        let engine_mesh = Mesh::new(ctx.device(), &desc)?;
+
+        ctx.add(engine_mesh, base_id);
+        Ok(())
     }
 }
 
